caab4977e9cf67a5c1ed05b5e86952c3494878ba
Shan Newton <shan.alexander2020@gmail.com>
2022-03-24 15:30:15 +0700

    adds rake keyword extractor, smart-stoplist of keywords to ignore, and adds keywords to the webpage schema

--- schemer.py
+++ schemer.py
@@ -2,30 +2,67 @@ import click
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import json 
+import rake #for SEO keyword extraction
 
 schema = {
     "@context": "https://schema.org/",
     "@graph": []
 }
 
+def get_page_keywords(soup):
+    stoppath = 'smart-stoplist.txt'
+    # Each word has at least 4 characters
+    # Each phrase has at most 4 words
+    # Each keyword appears in the text at least 2 times
+    rake_object = rake.Rake(stoppath, 4, 4, 2)
+    html_body = soup.body
+    # remove all javascript and stylesheet code
+    [x.extract() for x in html_body.find_all('script')]
+    [x.extract() for x in html_body.find_all('style')]
+    [x.extract() for x in html_body.find_all('meta')]
+    [x.extract() for x in html_body.find_all('noscript')]
+    body_string = html_body.get_text()
+    body_string_list = body_string.split()
+    body_string = ' '.join(body_string_list)
+    print("-------------")
+    print(body_string)
+    print("-------------")
+    keywords = rake_object.run(body_string)
+    # remove keyword-phrases less than 2 words
+    keywords = [x for x in keywords if len(x[0].split()) > 1]
+    keywords = [x[0] for x in keywords]
+    print("HTML Page Keywords:", keywords)
+    return keywords
+
 def get_soup():
-    print("hello")
+    print("Starting web driver...")
     driver = webdriver.Chrome('../../Downloads/chromedriver_win32/chromedriver')
     driver.get('https://www.pixelz.com')
-    results = []
     content = driver.page_source
-    
     soup = BeautifulSoup(content, features="lxml")
     return soup 
     
 def get_webpage_schema(soup):
+    print("Initializing WebPage Schema")
     results = []
     for element in soup.findAll("h1"):
         results.append(element.text)
-    page_name = results[0]
+    page_h1 = results[0]
+    # title = soup.find("meta", property="og:title")
+    title = soup.title
+    title = title.text if title else ""
+    description = soup.find("meta", property="og:description")
+    description = description["content"] if description else ""
+    url = soup.find("meta", property="og:url")
+    url = url["content"] if url else ""
+    keywords_list = get_page_keywords(soup)
     webpage_schema = {
         "@type": "WebPage",
-        "name": page_name}
+        "url": str(url),
+        "name": str(title),
+        "description": str(page_h1.replace("\n", " ")) + ", " + str(description),
+        "keywords": keywords_list
+        }
     return webpage_schema
 
 def print_schema(schema):
@@ -38,7 +75,7 @@ def print_schema(schema):
 @click.argument('url')
 def run(v,url):
     """Simple program that greets NAME for a total of COUNT times."""
-    click.echo(f"{url}")
+    click.echo(f"Initializing scrape of: {url}")
     soup = get_soup()
     schema["@graph"].append(get_webpage_schema(soup))
     print_schema(schema)
